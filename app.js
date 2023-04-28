13d95a41a9b12741e19f8a0a2e2af6e711456538
Luan Marques <luanfelipeb.marques@gmail.com>
2023-04-28 12:22:47 -0300

    body-parser

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false})); // apenas dados simples
+app.use(bodyParser.json()); // json de entrada no body
+
+app.use((req, res, next) => {
+    res.header('Acess-Contreol-Allow-Origin', '*');
+    res.header('Acess-Contreol-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+
+    if (req.method === 'OPTIONS') {
+        res.header('Acess-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({})
+    }
+    next();
+});
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
